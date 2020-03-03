3762b5d939535273987e5cfddc5389efc2e0cb55
Leandro Aparecido <leandro@backbase.com>
2020-03-03 17:35:51 +0100

    Forwarding requests to prism

--- src/main/java/com/backbase/api/simulator/prism/PrismServer.java
+++ src/main/java/com/backbase/api/simulator/prism/PrismServer.java
@@ -1,10 +1,12 @@
 package com.backbase.api.simulator.prism;
 
 import com.backbase.api.simulator.config.ApiSimulatorConfiguration;
+import com.google.common.io.ByteStreams;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.InterruptedIOException;
+import java.util.Enumeration;
 import java.util.concurrent.Executor;
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
@@ -13,6 +15,10 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.client.ClientHttpResponse;
+import org.springframework.web.client.ResponseErrorHandler;
+import org.springframework.web.client.RestTemplate;
 
 public class PrismServer {
 
@@ -21,6 +27,7 @@ public class PrismServer {
     private final ApiSimulatorConfiguration configuration;
     private final Executor executor;
     private final String applicationName;
+    private final RestTemplate restTemplate;
 
     private Process process;
 
@@ -30,11 +37,23 @@ public class PrismServer {
         this.configuration = configuration;
         this.executor = executor;
         this.applicationName = applicationName;
+        this.restTemplate = new RestTemplate();
+        this.restTemplate.setErrorHandler(new ResponseErrorHandler() {
+            @Override
+            public boolean hasError(ClientHttpResponse response) {
+                return false;
+            }
+
+            @Override
+            public void handleError(ClientHttpResponse response) {
+                throw new IllegalStateException("Not supposed to be called");
+            }
+        });
     }
 
     @PostConstruct
     public void start() throws IOException {
-        process = new ProcessBuilder("prism", "mock", "/home/leandro/dev/repo/cards-api/target/openapi.yaml", "-p",
+        process = new ProcessBuilder("src/etc/prism", "mock", "/home/leandro/dev/repo/cards-api/target/openapi.yaml", "-p",
             Integer.toString(configuration.getPort()))
             .start();
         executor.execute(() -> {
@@ -52,8 +71,27 @@ public class PrismServer {
     }
 
     public void forward(HttpServletRequest request, HttpServletResponse response) {
-        String pathPrefix = getPathPrefix();
-        String pathInfo = request.getPathInfo();
+        String prismPath = request.getPathInfo().replace(getPathPrefix(), "");
+        String url = String.format("http://localhost:%s/%s", configuration.getPort(), prismPath);
+
+        if (request.getQueryString() != null) {
+            url += "?" + request.getQueryString();
+        }
+
+        restTemplate.execute(url, HttpMethod.valueOf(request.getMethod()), clientRequest -> {
+            Enumeration<String> headerNames = request.getHeaderNames();
+            while (headerNames.hasMoreElements()) {
+                String headerName = headerNames.nextElement();
+                clientRequest.getHeaders().add(headerName, request.getHeader(headerName));
+            }
+            ByteStreams.copy(request.getInputStream(), clientRequest.getBody());
+        }, clientResponse -> {
+            response.setStatus(clientResponse.getRawStatusCode());
+            clientResponse.getHeaders()
+                .forEach((key, value1) -> value1.forEach(value -> response.addHeader(key, value)));
+            ByteStreams.copy(clientResponse.getBody(), response.getOutputStream());
+            return null;
+        });
     }
 
     private String getPathPrefix() {
