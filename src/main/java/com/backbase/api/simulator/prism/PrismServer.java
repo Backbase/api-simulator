313a653f1d664686ae483507c264836b0e041ff2
Leandro Aparecido <leandro@backbase.com>
2020-11-10 11:48:09 +0100

    Fix replace that was removing "/" from the paths
    Squashed commit of the following:
    
    commit 42a54e7620f5451e0b04644e0e0d106a25847d60
    Author: Rafael <rafaela@backbase.com>
    Date:   Tue Nov 10 11:19:38 2020 +0100
    
        Fix replace that was removing "/" from the paths
    
    commit 84e3aeb283b28dd88cc98b3f966a81e391ecad00
    Author: Rafael <rafaela@backbase.com>
    Date:   Tue Nov 10 10:12:48 2020 +0100
    
        Fix replace that was removing "/" from the paths
    
    commit 7145fbd520cb28005c7ff559cfca8f349923b112
    Author: Rafael <rafaela@backbase.com>
    Date:   Tue Nov 10 09:25:13 2020 +0100
    
        Fix replace that was removing "/" from the paths
    
    commit e37d7b74b2d9c9916e919e1d60f9aac4b641bae9
    Author: Rafael <rafaela@backbase.com>
    Date:   Mon Nov 9 14:21:35 2020 +0100
    
        Fix replace that was removing "/" from the paths

--- src/main/java/com/backbase/api/simulator/prism/PrismServer.java
+++ src/main/java/com/backbase/api/simulator/prism/PrismServer.java
@@ -109,11 +109,11 @@ public class PrismServer {
 
     private void doForward(HttpServletRequest request, HttpServletResponse response) {
         String prismPath = request.getPathInfo().replace(getPathPrefix(), "");
-        if (prismPath.equals(request.getPathInfo())) { // service-to-service call
+        if (!("/".equals(configuration.getBasePath()))
+            && prismPath.equals(request.getPathInfo())) { // service-to-service call
             prismPath = request.getPathInfo().replace(configuration.getBasePath(), "");
         }
         String url = String.format("http://localhost:%s/%s", configuration.getPort(), prismPath);
-
         if (request.getQueryString() != null) {
             url += "?" + request.getQueryString();
         }
